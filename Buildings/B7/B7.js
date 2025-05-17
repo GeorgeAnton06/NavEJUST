f777e9d18505220a8713eca03cd0711ba6f0f97b
George Anton <108414214+GeorgeAnton06@users.noreply.github.com>
2025-05-17 09:03:31 +0300

    Update B7.js

--- Buildings/B7/B7.js
+++ Buildings/B7/B7.js
@@ -16,7 +16,7 @@ function toggleSidebar() {
     if (sidebar.style.width === "250px") {
         sidebar.style.width = "0";
     } else {
-        sidebar.style.width = "250px";
+        sidebar.style.width = "249px";
     }
 }
 
